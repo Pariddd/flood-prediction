f1989e9a449b69f10a4e12ef701b9a9e8b98b622
Farid Kurniawan <farid.kurniawan0412@gmail.com>
2026-01-03 01:42:24 +0700

    perbarui tampilan dashboard dengan gambar latar baru, penyesuaian ukuran teks, dan optimasi warna indikator risiko

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,14 +6,14 @@
     <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <x-card class="lg:col-span-2 p-0 overflow-hidden relative">
             <div class="absolute inset-0 bg-cover bg-center opacity-50" 
-                 style="background-image: url('https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=800');">
+                 style="background-image: url('https://images.unsplash.com/photo-1548430065-53c58a6582dd?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');">
             </div>
             <div class="relative h-full text-white p-8 min-h-125 flex flex-col justify-between">
-                <div class="flex justify-center items-start mt-15">
-                    <div class="backdrop-blur-md rounded-3xl p-5">
-                        <p class="text-2xl font-semibold text-center">{{ $day }}</p>
-                        <p class="text-xl opacity-90 mt-1 text-center">{{ $date }}</p>
-                        <p class="mt-2 text-sm flex items-center justify-center gap-1.5">
+                <div class="flex justify-center items-start mt-10">
+                    <div class="p-5">
+                        <p class="text-3xl font-semibold text-center">{{ $day }}</p>
+                        <p class="text-2xl opacity-90 mt-1 text-center">{{ $date }}</p>
+                        <p class="mt-2 text-md flex items-center justify-center gap-1.5">
                             <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"/>
                             </svg>
@@ -22,7 +22,7 @@
                     </div>
                 </div>
                 <div class="text-center ">
-                    <div class="flex justify-center mb-6">
+                    <div class="flex justify-center mb-9">
                         <img
                             src="https://openweathermap.org/img/wn/{{ $currentIcon }}@4x.png"
                             alt="Current weather icon"
@@ -41,7 +41,7 @@
         <x-card class="flex flex-col gap-6">
             <div class="p-4 rounded-xl
                 @if($riskLevel === 'Bahaya') bg-red-500/20
-                @elseif($riskLevel === 'Waspada') bg-yellow-500/20
+                @elseif($riskLevel === 'Waspada') bg-yellow-400/50
                 @else bg-green-500/20
                 @endif
                 text-white text-center">
